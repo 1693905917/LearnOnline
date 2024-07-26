c96d44ea6e6e593c83f354032b734c799fdcf30f
1693905917 <1693905917@qq.com>
2024-07-26 14:26:53 +0800

    内容管理模块--修改课程

--- LearnOnline-content/LearnOnline-content-api/src/main/java/com/learnonline/content/api/CourseBaseInfoController.java
+++ LearnOnline-content/LearnOnline-content-api/src/main/java/com/learnonline/content/api/CourseBaseInfoController.java
@@ -5,6 +5,7 @@ import com.learnonline.base.model.PageParams;
 import com.learnonline.base.model.PageResult;
 import com.learnonline.content.model.dto.AddCourseDto;
 import com.learnonline.content.model.dto.CourseBaseInfoDto;
+import com.learnonline.content.model.dto.EditCourseDto;
 import com.learnonline.content.model.dto.QueryCourseParamsDto;
 import com.learnonline.content.model.po.CourseBase;
 import com.learnonline.content.service.CourseBaseInfoService;
@@ -12,9 +13,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @BelongsProject: LearnOnline
@@ -24,38 +23,61 @@ import org.springframework.web.bind.annotation.RestController;
  * @Description: 课程信息编辑接口
  * @Version: 1.0
  */
-@Api(value = "课程信息编辑接口",tags = "课程信息编辑接口")
+@Api(value = "课程信息编辑接口", tags = "课程信息编辑接口")
 @RestController
 public class CourseBaseInfoController {
     @Autowired
     CourseBaseInfoService courseBaseInfoService;
+
     /**
      * 课程查询接口
-     * @param pageParams 分页参数
+     *
+     * @param pageParams        分页参数
      * @param queryCourseParams 查询课程参数（可选）
      * @return 课程列表分页结果
      */
     @ApiOperation("课程查询接口")
     @PostMapping("/course/list")
-    public PageResult<CourseBase> list(PageParams pageParams, @RequestBody(required=false) QueryCourseParamsDto queryCourseParams){
+    public PageResult<CourseBase> list(PageParams pageParams, @RequestBody(required = false) QueryCourseParamsDto queryCourseParams) {
         PageResult<CourseBase> courseBasePageResult = courseBaseInfoService.queryCourseBaseList(pageParams, queryCourseParams);
         return courseBasePageResult;
     }
 
     /**
      * 新增课程基础信息
+     *
      * @param dto 课程基础信息的添加请求体，包括课程名称、课程描述等
      * @return CourseBaseInfoDto 课程基础信息的响应体，这里返回null仅作为示例，实际业务中应该返回创建后的课程基础信息
      */
     @ApiOperation("新增课程基础信息")
     @PostMapping("/course")
-    public CourseBaseInfoDto createCourseBase(@RequestBody @Validated(ValidationGroups.Insert.class) AddCourseDto dto){
+    public CourseBaseInfoDto createCourseBase(@RequestBody @Validated(ValidationGroups.Insert.class) AddCourseDto dto) {
         //机构id，由于认证系统没有上线暂时硬编码
         //TODO: 机构id从认证系统获取
         Long companyId = 1232141425L;
 //        int i = 1/0;
-        return courseBaseInfoService.createCourseBase(companyId,dto);
+        return courseBaseInfoService.createCourseBase(companyId, dto);
     }
 
+    /**
+     * 根据课程ID查询课程基础信息。
+     *
+     * <p>通过GET请求，并指定课程ID作为路径变量，查询并返回该课程的基础信息。</p>
+     *
+     * @param courseId 课程的唯一标识符ID。
+     * @return 返回包含课程基础信息的CourseBaseInfoDto对象。
+     */
+    @ApiOperation("根据课程id查询课程基础信息")
+    @GetMapping("/course/{courseId}")
+    public CourseBaseInfoDto getCourseBaseById(@PathVariable("courseId") Long courseId) {
+        return courseBaseInfoService.getCourseBaseInfo(courseId);
+    }
+    @ApiOperation("修改课程基础信息")
+    @PutMapping("/course")
+    public CourseBaseInfoDto modifyCourseBase(@RequestBody @Validated({ValidationGroups.Update.class}) EditCourseDto editCourseDto) {
+        //TODO 机构id，由于认证系统没有上线暂时硬编码
+        Long companyId = 1232141425L;
+        return courseBaseInfoService.updateCourseBase(companyId, editCourseDto);
+    }
 
 }
--- LearnOnline-content/LearnOnline-content-service/src/main/java/com/learnonline/content/service/impl/CourseBaseInfoServiceImpl.java
+++ LearnOnline-content/LearnOnline-content-service/src/main/java/com/learnonline/content/service/impl/CourseBaseInfoServiceImpl.java
@@ -10,6 +10,7 @@ import com.learnonline.content.mapper.CourseCategoryMapper;
 import com.learnonline.content.mapper.CourseMarketMapper;
 import com.learnonline.content.model.dto.AddCourseDto;
 import com.learnonline.content.model.dto.CourseBaseInfoDto;
+import com.learnonline.content.model.dto.EditCourseDto;
 import com.learnonline.content.model.dto.QueryCourseParamsDto;
 import com.learnonline.content.model.po.CourseBase;
 import com.learnonline.content.model.po.CourseCategory;
@@ -135,7 +136,7 @@ public class CourseBaseInfoServiceImpl implements CourseBaseInfoService {
         //一旦上面courseBaseMapper.insert(courseBaseNew);插入成功，courseBaseNew的Id属性就自动有了
         Long courseId = courseBaseNew.getId();
         BeanUtils.copyProperties(dto, courseMarketNew);
-        courseBaseNew.setId(courseId);
+        courseMarketNew.setId(courseId);
         int i = saveCourseMarket(courseMarketNew);
         if(i<=0){
             throw new LearnOnlineException("保存课程营销信息失败");
@@ -150,23 +151,73 @@ public class CourseBaseInfoServiceImpl implements CourseBaseInfoService {
      * @param courseId 课程ID
      * @return 课程基础信息DTO对象，若找不到对应的课程则返回null
      */
-    private CourseBaseInfoDto getCourseBaseInfo(Long courseId) {
+    @Override
+    public CourseBaseInfoDto getCourseBaseInfo(Long courseId){
+
         CourseBase courseBase = courseBaseMapper.selectById(courseId);
-        if(courseBase==null){
+        if(courseBase == null){
             return null;
         }
         CourseMarket courseMarket = courseMarketMapper.selectById(courseId);
         CourseBaseInfoDto courseBaseInfoDto = new CourseBaseInfoDto();
-        BeanUtils.copyProperties(courseBase, courseBaseInfoDto);
-        if(courseMarket!=null){
-            BeanUtils.copyProperties(courseMarket, courseBaseInfoDto);
+        BeanUtils.copyProperties(courseBase,courseBaseInfoDto);
+        if(courseMarket != null){
+            BeanUtils.copyProperties(courseMarket,courseBaseInfoDto);
         }
         //查询分类名称
-        CourseCategory courseCategoryBySt  = courseCategoryMapper.selectById(courseBase.getSt());
+        CourseCategory courseCategoryBySt = courseCategoryMapper.selectById(courseBase.getSt());
         courseBaseInfoDto.setStName(courseCategoryBySt.getName());
-        CourseCategory courseCategoryByMt  = courseCategoryMapper.selectById(courseBase.getMt());
+        CourseCategory courseCategoryByMt = courseCategoryMapper.selectById(courseBase.getMt());
         courseBaseInfoDto.setMtName(courseCategoryByMt.getName());
         return courseBaseInfoDto;
+    }
+
+
+    /**
+     * 更新课程基础信息。
+     *
+     * <p>此方法用于根据提供的公司ID和课程编辑DTO（数据传输对象）来更新指定课程的基础信息。
+     * 首先，根据课程ID查询课程基础信息，如果不存在则抛出异常。
+     * 然后，验证当前公司是否有权修改该课程（即课程是否属于当前公司），如果不是则抛出异常。
+     * 接着，将编辑DTO中的信息复制到课程基础对象中，并更新修改时间。
+     * 更新课程基础信息后，如果更新操作未成功（即影响行数为0或负数），则抛出异常。
+     * 同时，还会处理课程的营销信息，将其保存到数据库中。
+     * 最后，重新查询并返回更新后的课程基础信息DTO。</p>
+     *
+     * @param companyId 公司的唯一标识符ID，用于验证课程是否属于该公司。
+     * @param dto       包含要更新的课程基础信息的EditCourseDto对象。
+     * @return 返回更新后的课程基础信息DTO对象。
+     * @throws LearnOnlineException 如果课程不存在、当前公司无权修改该课程或更新操作失败，则抛出此异常。
+     */
+    @Override
+    @Transactional
+    public CourseBaseInfoDto updateCourseBase(Long companyId, EditCourseDto dto) {
+        Long courseId = dto.getId();
+        CourseBase courseBase = courseBaseMapper.selectById(courseId);
+        if(courseBase==null){
+            LearnOnlineException.cast("课程不存在");
+        }
+        //根据具体的业务逻辑只能去service层校验
+        //校验本机构只能修改本机构的课程
+        if(!courseBase.getCompanyId().equals(companyId)){
+            LearnOnlineException.cast("本机构只能修改本机构的课程");
+        }
+
+        //封装基本信息的数据
+        BeanUtils.copyProperties(dto, courseBase);
+        courseBase.setChangeDate(LocalDateTime.now());
+
+        //更新课程基本信息
+        int i = courseBaseMapper.updateById(courseBase);
+        if(i<=0){
+            LearnOnlineException.cast("更新课程基本信息失败");
+        }
+        //封装营销信息的数据
+        CourseMarket courseMarket = new CourseMarket();
+        BeanUtils.copyProperties(dto, courseMarket);
+        saveCourseMarket(courseMarket);
+        //查询课程信息
+        return this.getCourseBaseInfo(courseId);
     }
 
     /**
@@ -177,28 +228,28 @@ public class CourseBaseInfoServiceImpl implements CourseBaseInfoService {
      * @throws RuntimeException 如果收费规则没有选择，或者收费规则为收费但价格没有填写或填写了非法数字，则抛出异常
      */
     //保存课程营销信息
-    private int saveCourseMarket(CourseMarket courseMarketNew) {
+    private int saveCourseMarket(CourseMarket courseMarketNew){
         //收费规则
         String charge = courseMarketNew.getCharge();
-        if (StringUtils.isBlank(charge)) {
-            throw new LearnOnlineException("收费规则没有选择");
+        if(StringUtils.isBlank(charge)){
+            LearnOnlineException.cast("收费规则没有选择");
         }
         //收费规则为收费，但是价格没有填写或者填了一个非法数字
-        if (charge.equals("201001")) {
-            if (courseMarketNew.getPrice() == null || courseMarketNew.getPrice().floatValue() <= 0) {
-                throw new LearnOnlineException("收费规则为收费，但是价格没有填写或者填了一个非法数字");
+        if(charge.equals("201001")){
+            if(courseMarketNew.getPrice() == null || courseMarketNew.getPrice().floatValue()<=0){
+                LearnOnlineException.cast("课程为收费价格不能为空且必须大于0");
             }
         }
         //根据id从课程营销表查询
         CourseMarket courseMarketObj = courseMarketMapper.selectById(courseMarketNew.getId());
-        //从数据库查询营销信息，存在则更新，不存在则添加
+//从数据库查询营销信息，存在则更新，不存在则添加
         if(courseMarketObj == null){
-            //新增
             return courseMarketMapper.insert(courseMarketNew);
         }else{
-            BeanUtils.copyProperties(courseMarketNew, courseMarketObj);
+            BeanUtils.copyProperties(courseMarketNew,courseMarketObj);
             courseMarketObj.setId(courseMarketNew.getId());
             return courseMarketMapper.updateById(courseMarketObj);
         }
     }
+
 }
